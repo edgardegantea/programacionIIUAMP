959035476ff01dc7037d595a3c48dd5ec5a26aed
edgardegantea <edgar.degante.a@gmail.com>
2022-05-17 17:51:22 -0500

    Nuevos cambios

--- ejercicio02/clases.js
+++ ejercicio02/clases.js
@@ -3,7 +3,6 @@ function Persona(nombre, apellidos) {
   // Atributos de la clase Persona()
   this.nombre = nombre;
   this.apellidos = apellidos;
-  // alert("Objeto " + nombre + " " + apellidos);
 }
 
 // Crear un método para la clase Persona()
@@ -11,13 +10,14 @@ Persona.prototype.Saludar = function() {
   alert("Hola, soy " + this.nombre);
 }
 
-var nombre = prompt("Ingresa tu nombre: ");
-var apellidos = prompt("Ingresa tus apellidos: ");
+// Método para la imprimir nombre
+Persona.prototype.ImprimirNombre = function () {
+  var nombre = prompt("Ingresa tu nombre: ");
+  var apellidos = prompt("Ingresa tus apellidos: ");
+  // Objetos de la clase Persona
+  persona1 = new Persona(nombre, apellidos);
+  alert('Persona: ' + persona1.nombre + " " + persona1.apellidos);
+};
 
-// Objetos de la clase Persona
-persona1 = new Persona(nombre, apellidos);
-
-// Invocar los atributos de la clase Persona() por medio del objeto
-// Objeto: persona1
-alert("Objeto 1: " + persona1.nombre + " " + persona1.apellidos);
+persona1.ImprimirNombre();
 persona1.Saludar();
